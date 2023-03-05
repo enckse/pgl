e94ab9935e435a3ec46521f245f6b58dc7bcb7dc
Sean Enck <enckse@voidedtech.com>
2023-03-04 21:30:21 -0500

    set can be a special form of Map

--- types/collections/set.go
+++ types/collections/set.go
@@ -5,16 +5,13 @@ package collections
 type (
 	// Set is a simple map-based set via map[T]struct{}
 	Set[T comparable] struct {
-		data map[T]struct{}
+		Map[T, struct{}]
 	}
 )
 
 // Add will add an item to the set
 func (s *Set[T]) Add(item T) {
-	if s.data == nil {
-		s.data = make(map[T]struct{})
-	}
-	s.data[item] = struct{}{}
+	s.Set(item, struct{}{})
 }
 
 // Clear will reset the set
@@ -24,17 +21,11 @@ func (s *Set[T]) Clear() {
 
 // Remove will remove an item from the set
 func (s *Set[T]) Remove(item T) {
-	if !s.Contains(item) {
-		return
-	}
-	delete(s.data, item)
+	s.Delete(item)
 }
 
 // Contains will indicate if the set contains a value
 func (s *Set[T]) Contains(item T) bool {
-	if s.data == nil {
-		return false
-	}
-	_, ok := s.data[item]
+	_, ok := s.Get(item)
 	return ok
 }
